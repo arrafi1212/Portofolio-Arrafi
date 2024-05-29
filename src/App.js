4f527362f83cb700d2c3d46fc2cdfdb08c741818
arrafi1212 <arrafifakhri1@gmail.com>
2024-05-29 12:43:37 +0700

    Baru

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef } from "react";
+import Typed from "typed.js";
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
+  const typedElement = useRef(null);
+
+  useEffect(() => {
+    const options = {
+      strings: ["Web Developer", "Data Scientist"],
+      typeSpeed: 40,
+      backSpeed: 40,
+      loop: true,
+    };
+
+    const typed = new Typed(typedElement.current, options);
+
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <div id="stars"></div>
+      <div id="stars2"></div>
+      <div id="stars3"></div>
+
+      <div className="welcome">
+        <h1 className="txt-depan text-start pl-20 w-11/12 text-6xl font-semibold">
+          Hi, <br />
+          I'm <span className="nama">Arrafi</span>
+          <br />
+          <span ref={typedElement} className="typed-element font-normal"></span>
+        </h1>
+      </div>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import "../styles/Navbar.css";
+// import logoSekolah from "../assets/ananda.png";
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div>
+      <nav className="">
+        <div className="navbar-container">
+          <button
+            className={`hamburger hamburger--collapse ${
+              isOpen ? "is-active" : ""
+            }`}
+            type="button"
+            onClick={toggleNavbar}
+          >
+            <span className="hamburger-box">
+              <span className="hamburger-inner"></span>
+            </span>
+          </button>
+          <ul className={`navbar-menu ${isOpen ? "open" : ""}`}>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="./Rules">My Work</a>
+            </li>
+            {/* <li>
+              <a href="./Pertandingan">Contest</a>
+            </li> */}
+            <li>
+              <a href="./About Me">About Me</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
